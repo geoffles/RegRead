2a0b15827f9f08e9dc40b680488e440588060b59
geoffles <geoffles@gmail.com>
2015-10-20 09:40:03 +0200

    Added RegRead - reg.exe read emulator Program. Add a readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# RegRead fix for VS Command Prompt
+
+In somecases, registry editing may be blocked by a group policy which then blocks your VS Command Prompt.
+
+VS Command Prompt attempts to get settings out of the registry by using reg.exe, which denies access to the registry.
+
+The simple solution is to replace your reg.exe
+
+## Installation
+
+This is for VS 2012. For other versions, you'll have to adapt.
+
+There are three files which you will need to edit:
+
+-  %VS110COMNTOOLS%\VCVarsQueryRegistry.bat
+-  %VS110COMNTOOLS%\VsDevCmd.bat
+-  %VS110COMNTOOLS%\vsvars32.bat
+
+### Instructions
+
+1. Unpack RegRead.exe to your somewhere in your %PATH% variable
+  o  You can create a create a directory (eg, Program Files\RegRead) and add it to the your PATH variable
+2. For each of the above files,
+  1.  Open the file
+  2.  Goto 'Find and Replace'
+  3.  Find `'reg query` , replace with `'RegRead.exe query`
+  4.  Test!
+  
+Happy CLIing.
---
+++ RegRead/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Security.Permissions;
+
+namespace RegRead
+{
+    class Program
+    {
+        
+        static int Main(string[] args)
+        {
+            try
+            {
+                var targetRegistry = args[1].Substring(0, 4);
+                var targetKey = args[1].Substring(5);
+
+                string targetValue = null;
+                if (args[2].ToLower() == "/v")
+                {
+                    targetValue = args[3];
+                }
+                else
+                {
+                    return 1;
+                }
+
+                var hkey = targetRegistry == "HKLM" ? Registry.LocalMachine : Registry.CurrentUser;
+                var key = hkey.OpenSubKey(targetKey);
+
+
+
+                var result = key.GetValue(targetValue);
+                Console.WriteLine();
+                Console.WriteLine(key.Name);
+                Console.WriteLine("    {0}    REG_SZ    {2}", targetValue, key.GetValueKind(targetValue), result);
+                Console.WriteLine();
+                Console.WriteLine();
+
+                return 0;
+            }
+            catch
+            {
+                return 1;
+            }
+        }
+    }
+}
